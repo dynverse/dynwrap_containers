ca95cad611fb4e6c725c2abc582194499e504396
Robrecht Cannoodt <rcannood@gmail.com>
2019-03-08 10:05:44 +0100

    renaming dynwrappy3 to dynwrappy

--- dynwrappy_tester/example.R
+++ dynwrappy_tester/example.R
@@ -3,7 +3,7 @@
 # generate dataset with certain seed
 set.seed(1)
 data <- dyntoy::generate_dataset(
-  id = "specific_example/angle",
+  id = "specific_example/dynwrappy",
   num_cells = 300,
   num_features = 250,
   model = "cyclic"
